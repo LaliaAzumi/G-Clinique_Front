87a154430ea54b1aeede5f3d17c275ebb9c2a922
LaliaAzumi <138109197+LaliaAzumi@users.noreply.github.com>
2026-04-01 19:26:25 +0300

    integration secretaire
    affichage liste secretaire

--- src/types/secretary.ts
+++ src/types/secretary.ts
@@ -2,6 +2,7 @@ export interface Secretary {
   id: string;
   firstName: string;
   lastName: string;
+  username: string;
   email: string;
   phone: string;
   assignedService: string; // ex: Cardiologie, Urgences, Accueil
