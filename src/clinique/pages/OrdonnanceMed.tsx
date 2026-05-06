8ef95593b71d921dd9f3394fd75e6cf096d8bff0
Nekena <nekenarakotomalala760@gmail.com>
2026-05-06 10:54:12 +0300

    feat MED-SEC : update front ordonnance

--- src/clinique/pages/OrdonnanceMed.tsx
+++ src/clinique/pages/OrdonnanceMed.tsx
@@ -314,43 +314,48 @@ const OrdonnanceMed = () => {
   }, [ordonnances, filterId, filterDate, filterPaid]);
 
   return (
-    <div className="page-container p-6 min-h-screen">
+    <div className="page-container">
       <div className="page-header mb-6">
-        <h1 className="text-2xl font-bold text-gray-800 flex items-center gap-2">
-          <FileText className="text-blue-600" /> Gestion des Ordonnances
-        </h1>
+          <div>
+          <h1 className="page-title">Historique des Ordonnances</h1>
+          <p className="breadcrumb">Ordonnances › Historiques </p>
+        </div>
       </div>
 
       {/* RECHERCHE */}
       <div className="table-toolbar mb-3 flex gap-3 p-4 rounded-xl shadow-sm">
-        <div className="relative flex-1">
-          <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 size-5" />
-          <input
+        
+        <div className="search-input">
+            <Search size={15} className="search-icon" />
+            <input
             type="text"
-            className="w-full pl-10 pr-4 py-2 border rounded-lg"
+            // className="w-full pl-10 pr-4 py-2 border rounded-lg"
+            className="w-full pl-10 pr-4 py-2 border border-gray-200 rounded-lg outline-none text-gray-900"
             placeholder="Rechercher par nom de patient..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             onKeyDown={(e) => e.key === "Enter" && handleSearch()}
           />
-        </div>
+          </div>
 
         <button
           onClick={handleSearch}
           className="bg-blue-600 text-white px-6 py-2 rounded-lg"
+              style={{ backgroundColor: '#1fb468', borderColor: '#1fb468', padding:"15px 15px" }} 
         >
           Rechercher
         </button>
       </div>
 
       {/* FILTRES */}
-      <div className="mb-6 flex flex-wrap gap-3 bg-white p-4 rounded-xl shadow-sm">
+      <div className="mb-6 flex flex-wrap gap-3 p-4 rounded-xl shadow-sm">
 
         {/* ID */}
         <input
           type="text"
           placeholder="Filtrer par ID"
           className="border px-3 py-2 rounded-lg"
+          style={{background:"#f8f8f81e", color:"#000000f"}}
           value={filterId}
           onChange={(e) => setFilterId(e.target.value)}
         />
@@ -359,6 +364,7 @@ const OrdonnanceMed = () => {
         <input
           type="date"
           className="border px-3 py-2 rounded-lg"
+          style={{background:"#f8f8f81e", color:"#c0c0c0"}}
           value={filterDate}
           onChange={(e) => setFilterDate(e.target.value)}
         />
@@ -366,12 +372,13 @@ const OrdonnanceMed = () => {
         {/* PAYÉ */}
         <select
           className="border px-3 py-2 rounded-lg"
+          style={{background:"#f8f8f81e", color:"#c0c0c0"}}
           value={filterPaid}
           onChange={(e) => setFilterPaid(e.target.value as any)}
         >
-          <option value="ALL">Tous</option>
-          <option value="PAID">Payés</option>
-          <option value="UNPAID">Non payés</option>
+          <option  style={{background:"#f8f8f81e", color:"#424242"}} value="ALL">Tous</option>
+          <option style={{background:"#f8f8f81e", color:"#424242"}} value="PAID">Payés</option>
+          <option  style={{background:"#f8f8f81e", color:"#424242"}} value="UNPAID">Non payés</option>
         </select>
 
         {/* RESET */}
@@ -382,6 +389,7 @@ const OrdonnanceMed = () => {
             setFilterPaid("ALL");
           }}
           className="px-4 py-2 bg-gray-100 rounded-lg"
+            style={{background:"#818181", color:"#ffffff"}}
         >
           Reset
         </button>
@@ -399,33 +407,50 @@ const OrdonnanceMed = () => {
           <p className="col-span-full text-center py-10 text-gray-500">Chargement...</p>
         ) : filteredOrdonnances.length > 0 ? (
           filteredOrdonnances.map((ord) => (
-            <div key={ord.id} className="bg-white p-5 rounded-xl shadow-sm border hover:shadow-md">
+            <div key={ord.id} className="bg-white p-5 rounded-xl shadow-sm border hover:shadow-md" style={{background:"#0706063b", borderColor:"#c0c0c0"}}>
 
               <div className="flex justify-between">
                 <div>
                   <h3 className="font-bold text-lg">
-                    {getPatientName(ord)} #{ord.id}
+                    {getPatientName(ord)} 
+                    <br/>
+                    #{ord.id}
                   </h3>
                   <p className="text-sm text-gray-500">
                     {getOrdonnanceDate(ord)
                       ? new Date(getOrdonnanceDate(ord)!).toLocaleDateString("fr-FR")
                       : "Date inconnue"}
+                      
                   </p>
                 </div>
 
-                <span className={`px-3 py-1 rounded-full text-xs flex items-center gap-1 ${
+                {/* <span 
+                style={{padding:"2px 2px 2px 2px"}}
+                className={`px-3 py-1 text-xs flex items-center gap-1 ${
                   ord.paye ? "bg-green-100 text-green-700" : "bg-red-100 text-red-700"
                 }`}>
                   {ord.paye ? <CheckCircle size={14} /> : <XCircle size={14} />}
                   {ord.paye ? "Payé" : "Non payé"}
+                </span> */}
+                <span
+                className={`inline-flex items-center gap-1 px-2 py-0 text-[11px] leading-none font-medium rounded-md ${
+                    ord.paye
+                    ? "bg-green-100 text-green-700"
+                    : "bg-red-100 text-red-700"
+                }`}
+                >
+                {ord.paye ? <CheckCircle size={12} /> : <XCircle size={12} />}
+                <span className="leading-none">{ord.paye ? "Payé" : "Non payé"}</span>
                 </span>
               </div>
-
-              <div className="flex gap-2 mt-6">
+                <br/>
+                <br/>
+              {/* <div className="flex gap-2 mt-6">
 
                 <button
                   onClick={() => window.open(getOrdonnancePdfUrl(ord), "_blank")}
                   className="flex-1 bg-gray-100 py-2 rounded-lg"
+                  style={{background:"#f8f8f81e", color:"#c0c0c0"}}
                 >
                   <Eye size={16} /> PDF
                 </button>
@@ -438,7 +463,30 @@ const OrdonnanceMed = () => {
                     <CreditCard size={16} /> Encaisser
                   </button>
                 )}
-              </div>
+              </div> */}
+              <div className="flex gap-2 mt-6">
+                
+
+                <button
+                    onClick={() => window.open(getOrdonnancePdfUrl(ord), "_blank")}
+                    className="flex-1 flex items-center justify-center gap-2 py-2 rounded-lg border border-gray-200 bg-gray-50 text-gray-700 hover:bg-gray-100 transition"
+                    style={{background:"#8a8989",borderColor:"#4e4e4e", color:"#ffffff"}}
+                >
+                    <Eye size={16} />
+                    <span className="text-sm font-medium">PDF</span>
+                </button>
+
+                {!ord.paye && userRole === "SECRETAIRE" && (
+                    <button
+                    onClick={() => handleMarkAsPaid(ord.id)}
+                    className="flex-1 flex items-center justify-center gap-2 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700 transition"
+                    >
+                    <CreditCard size={16} />
+                    <span className="text-sm font-medium">Encaisser</span>
+                    </button>
+                )}
+
+                </div>
             </div>
           ))
         ) : (
